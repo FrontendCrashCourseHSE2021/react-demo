455a710b562b2a47c9348b2aaffa2fceab681935
Semyon Danilov <samvimes@yandex.ru>
2021-11-09 17:17:08 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button, Card, Col, Container, Nav, Navbar, NavDropdown, Row} from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <Navbar bg="light" expand="lg">
+              <Container>
+                  <Navbar.Brand href="#home">React Demo</Navbar.Brand>
+                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                  <Navbar.Collapse id="basic-navbar-nav">
+                      <Nav className="me-auto">
+                          <Nav.Link href="#">Home</Nav.Link>
+
+                          <Nav.Link href="#">Link</Nav.Link>
+
+                          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                              <NavDropdown.Item href="#">Test</NavDropdown.Item>
+                              <NavDropdown.Divider />
+                              <NavDropdown.Item href="#">Another test</NavDropdown.Item>
+                          </NavDropdown>
+                      </Nav>
+                  </Navbar.Collapse>
+              </Container>
+          </Navbar>
+
+          <Container>
+              <Row>
+                  <Col>
+                      <Card style={{ width: '18rem' }}>
+                          <Card.Img variant="top" src="https://engineering.fb.com/wp-content/uploads/2016/09/gjak3qbrgsjmr1afaaaaaada9cc2bj0jaaab.jpg" />
+                          <Card.Body>
+                              <Card.Title>Card Title</Card.Title>
+                              <Card.Text>
+                                  Some quick example text to build on the card title and make up the bulk of
+                                  the card's content.
+                              </Card.Text>
+                              <Button variant="primary">Go somewhere</Button>
+                          </Card.Body>
+                      </Card>
+                  </Col>
+              </Row>
+
+              <br/>
+
+              <Row>
+                  <Col>
+                      <Button variant={"primary"} onClick={() => alert(1)}>test</Button>
+                  </Col>
+              </Row>
+          </Container>
+      </div>
   );
 }
 
